722f5f26ed9b34b1b1f9dd5fce7e5cb3b6d4f2c4
Will Barlow <will@threadtheweb.co.uk>
2015-03-10 02:46:12 +0000

    CLI Arguments, redone
    Reimplemented CLI arguments. Added placeholder ‘print()’ for each
    argument.

--- sqlsploit/cli.py
+++ sqlsploit/cli.py
@@ -2,11 +2,19 @@ import click
 
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.option('--keyword', '-k', is_flag=True, help='Keyword for searching')
-@click.option('--searchtype', '-t', type=click.Choice(['Default', 'Pattern', 'Smart', 'Keyword']))
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """SQL Data discovery tool."""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+@click.option('--host', '-h', default='127.0.0.1', required=True, help='Host/IP')
+@click.option('--username', '-u',default='sa', required=False, help='Username')
+@click.option('--password', '-p', default='', hide_input=True, prompt=True, required=False, help='Password')
+@click.option('--dbtype', '-db', default='MSSQL', type=click.Choice(['MySQL', 'MSSQL', 'Oracle', 'PG']), required=False, help='DB Type')
+@click.option('--keyword', '-k', default='password', required=False, help='Search Keyword')
+@click.option('--searchtype', '-s', default='Default', type=click.Choice(['Default', 'Pattern', 'Smart', 'Keyword']), required=False, help='Search Type')
+@click.option('--version', '-V', default='0.1.0', required=False, help='SQLSploit Version')
+def main(host, username, password, dbtype, keyword, searchtype, version):
+    """SQLSploit - Data Extraction Toolkit"""
+    click.echo('Host: %s' % host)
+    click.echo('Username: %s' % username)
+    click.echo('Password: %s' % password)
+    click.echo('dbtype: %s' % dbtype)
+    click.echo('Keyword: %s' % keyword)
+    click.echo('searchtype: %s' % searchtype)
+    click.echo('Version: %s' % version)
